d8122d9446ed7cf663b63e58d1a9ba713db0dee4
nyamekye17 <122738782+nyamekye17@users.noreply.github.com>
2023-04-11 10:12:19 -1200

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-   Weather
+   Weather App
+   <footer>
+    This project was created by Nana Aba Acquah and is {" "}
+   <a href="https://github.com/nyamekye17/react-weather-app" target="_blank" rel="noreferrer" >open-sourced on Github</a>
+   </footer>
     </div>
   );
 }
